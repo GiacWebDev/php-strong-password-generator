0d266df250b83b8484f82bc0f1e4fca34d0a57af
GiacWebDev <giacobechini@gmail.com>
2023-10-28 16:44:59 +0200

    [edit] generate function + stamp password

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php 
-  $limit = 50;
-  $password = '';
 
   // effettuo un controllo con if isset
   if(isset($_POST['submit'])) {
@@ -8,18 +6,26 @@
   }
 
   
-  function generateRandomPassword($length) {
+  function generateRandomPassword($Length) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{}|;:,.<>?';
+    // definisco una lunghezza variabile dei caratteri
+    $charactersLength = strlen($characters) -1;
+    // creo una variabile vuota che vado a riempire 
     $password = '';
-    $max = strlen($characters) - 1;
-
-    for ($i = 0; $i < $length; $i++) {
-        $password .= $characters[rand(0, $max)];
+  
+    for($i = 0; $i < $Length; $i++) {
+      $randomCharacter = $characters[rand(0, $charactersLength)];
+      // aggiungi e mantieni
+      $password .= $randomCharacter;
     }
-
+    // adesso questo return ha i caratteri all'interno
     return $password;
+  
   }
-  ?>
+
+  $passwordLength = $_GET['numbers']; 
+
+ ?>
 
 
 
@@ -48,38 +54,34 @@
       <h3>Genera una password sicura</h3>
     </div>
 
+    <!-- STAMPA PASSWORD / ERRORE -->
     <div class="row message-row">
       <div class="col">
-        <p>Scegliere una password con un minimo di 8 caratteri e un massimo di 32 caratteri</p>
+      <?php
+      if($passwordLength > 8 && $passwordLength < 32) {
+      $createdPassword = generateRandomPassword($passwordLength);
+      
+      echo '<p>Password creata:' . $createdPassword . '</p>'; 
+      }else{
+        echo '<p>La lunghezza della password deve essere di almeno 8 e massimo 32 caratteri</p>';
+      } ?>
       </div>
     </div>
 
     <div class="row info py-5 px-3">
-      <div class=" col d-flex ">
+      <div class=" col">
 
-        <div class="div">
+        <form action="index.php" method="GET">
           <h4>Lunghezza password</h4>
           <button type="submit" class="btn btn-primary ">Invia</button>
           <button class="btn btn-secondary ">Annulla</button>
-        </div>
-
-        <!-- Creo lista numeri con ciclo for -->
-        <div class="div">
-          <form action="index.php" method="post">
-            <select class="form-select" name="numbers">
-              <?php for ($i = 0; $i < $limit; $i++) { 
-                echo "<option>$i</option>";
-              } ?>
-            </select>
-          </form>
-        </div>
+          
+          <input type="number" id="numbers" name="numbers" require>
+        </form>
 
       </div>
 
     </div>
-
-    <h4>La password è: <?php echo $password; ?></h4>
-
 
   </div>
   
